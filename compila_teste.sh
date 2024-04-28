7639023233aa5532149e1934196efa99d2dd1513
Nicholas Zortea Graczik <zorteanicholas619@gmail.com>
2024-04-28 02:19:21 -0300

    Remoção de arquivos desnecessários!

--- compila_teste.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm teste_fila
-gcc fila.c testa_fila.c -o teste_fila
-./teste_fila
-
--- executaCobra.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm cobrinha
-gcc cobra.c fila.c tecla.c tela.c -o cobrinha
-./cobrinha
